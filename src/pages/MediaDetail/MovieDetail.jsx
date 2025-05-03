6e1d22cc6b8d13899e8b0b4f09d9f23d680e2011
Godspower Amanze <59035803+leo9947@users.noreply.github.com>
2025-05-03 17:59:05 +0100

    partial update to the movie details page (added language,country and genre)

--- src/pages/MediaDetail/MovieDetail.jsx
+++ src/pages/MediaDetail/MovieDetail.jsx
@@ -10,7 +10,75 @@ const MovieDetail = () => {
     <div className="font-Montserrat">
       {data && (
         <div className="p-6">
-          <p className="font-semibold sm:font-bold tracking-tighter text-lg sm:text-2xl text-center">
+          <div className="hidden md:flex w-[70%] ml-[5%]">
+            <img
+              className=" mt-4 rounded-md min-h-[350px] max-h-[400px] object-contain object-center"
+              src={
+                data.backdrop_path
+                  ? `https://image.tmdb.org/t/p/original/${data.details?.backdrop_path}`
+                  : `https://image.tmdb.org/t/p/original/${data.details?.poster_path}`
+              }
+              alt=""
+            />
+            <div className="text-base text-justify p-3 ml-5 flex flex-col items-start mt-[8%] justify-between">
+              <div>
+                <p className="font-bold tracking-tighter text-2xl ">
+                  {data?.details.title ||
+                    data?.details.name ||
+                    data?.details.original_name}{" "}
+                  (
+                  {data?.details?.release_date?.slice(0, 4) ||
+                    data?.details?.first_air_date?.slice(0, 4)}
+                  )
+                </p>
+                {data.details.runtime ? (
+                  <p className="italic text-sm text-center tracking-tighter">
+                    {`${Math.floor(data.details.runtime / 60)}hr
+                ${Math.floor(data.details.runtime % 60)}mins`}
+                  </p>
+                ) : (
+                  ""
+                )}
+              </div>
+              <div className="font-semibold space-y-3">
+                <p>
+                  Release Date:{" "}
+                  <span className="font-normal">
+                    {data?.details?.first_air_date ||
+                      data?.details?.release_date}
+                  </span>
+                </p>
+                <p>
+                  Country:{" "}
+                  {data?.details?.production_countries.map((country, i) => (
+                    <span key={i} className="font-normal">
+                      {`${country.name}, `}
+                    </span>
+                  ))}
+                </p>
+                <p>
+                  Language:{" "}
+                  {data?.details?.spoken_languages.map((language, i) => (
+                    <span key={i} className="font-normal">
+                      {`${language.name}, `}
+                    </span>
+                  ))}
+                </p>
+                <p>
+                  Genres:{" "}
+                  {data?.details?.genres.map((genre, i) => (
+                    <span key={i} className="font-normal">
+                      {`${genre.name}, `}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile screens up to small screens */}
+
+          <p className="font-semibold sm:font-bold tracking-tighter text-lg sm:text-2xl text-center md:hidden">
             {data?.details.title ||
               data?.details.name ||
               data?.details.original_name}{" "}
@@ -21,7 +89,7 @@ const MovieDetail = () => {
           </p>
           <div>
             {data.details.runtime ? (
-              <p className="italic text-sm text-center tracking-tighter">
+              <p className="italic text-sm text-center tracking-tighter md:hidden">
                 {`${Math.floor(data.details.runtime / 60)}hr
                 ${Math.floor(data.details.runtime % 60)}mins`}
               </p>
@@ -29,43 +97,71 @@ const MovieDetail = () => {
               ""
             )}
           </div>
-          {/* <div className="hidden md:flex w-[70%] mx-auto">
-            <img
-              className=" mt-4 rounded-md  mx-auto h-[250px] sm:h-[350px] md:h-[450px] object-contain object-center"
-              src={
-                data.backdrop_path
-                  ? `https://image.tmdb.org/t/p/original/${data.details?.backdrop_path}`
-                  : `https://image.tmdb.org/t/p/original/${data.details?.poster_path}`
-              }
-              alt=""
-            />
-            <p className="text-base text-justify p-3 ml-3 flex justify-center items-center">
-              {data.details.overview}
-            </p>
-          </div> */}
+
+          <div className="flex mt-2 md:hidden w-[80%] mx-auto gap-4 tracking-tighter ">
+            <div className="">
+              <img
+                className=" mt-4 rounded-md  mx-auto min-h-[240px] max-h-[600px] sm:h-[300px] object-contain object-center md:hidden"
+                src={
+                  data.backdrop_path
+                    ? `https://image.tmdb.org/t/p/original/${data.details?.backdrop_path}`
+                    : `https://image.tmdb.org/t/p/original/${data.details?.poster_path}`
+                }
+                alt=""
+              />
+            </div>
+            <div className="">
+              <div className="font-semibold text-xs space-y-2 sm:space-y-4 flex flex-col justify-end h-full">
+                <p>
+                  Release Date:{" "}
+                  <span className="font-normal">
+                    {data?.details?.first_air_date ||
+                      data?.details?.release_date}
+                  </span>
+                </p>
+                <p>
+                  Country:{" "}
+                  {data?.details?.production_countries.map((country, i) => (
+                    <span key={i} className="font-normal">
+                      {`${country.name}, `}
+                    </span>
+                  ))}
+                </p>
+                <p>
+                  Language:{" "}
+                  {data?.details?.spoken_languages.map((language, i) => (
+                    <span key={i} className="font-normal">
+                      {`${language.name}, `}
+                    </span>
+                  ))}
+                </p>
+                <p>
+                  Genres:{" "}
+                  {data?.details?.genres.map((genre, i) => (
+                    <span key={i} className="font-normal">
+                      {`${genre.name}, `}
+                    </span>
+                  ))}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* For All screens */}
 
           <div className="">
-            <img
-              className=" mt-4 rounded-md  mx-auto h-[250px] sm:h-[350px] md:h-[450px] object-contain object-center"
-              src={
-                data.backdrop_path
-                  ? `https://image.tmdb.org/t/p/original/${data.details?.backdrop_path}`
-                  : `https://image.tmdb.org/t/p/original/${data.details?.poster_path}`
-              }
-              alt=""
-            />
-            <div className="w-[90%] mx-auto">
+            <div className="w-[97%] sm:w-[90%] mx-auto">
               <p className="font-bold sm:text-lg text-base pt-4">Overview</p>
               <p className="text-base text-justify pt-2">
                 {data.details.overview}
               </p>
             </div>
-            <div className="w-[90%] mx-auto">
+            <div className="w-[97%] sm:w-[90%] mx-auto">
               {data?.trailer ? (
                 <div>
                   <p className="font-bold sm:text-lg text-base pt-4">Trailer</p>
                   <iframe
-                    className="pt-3 w-[100%] md:w-[70%] sm:w-[80%] h-[250px] md:h-[300px] mx-auto"
+                    className="pt-3 w-[100%] md:w-[70%] sm:w-[80%] h-[260px] sm:h-[360px] md:h-[430px] mx-auto"
                     src={`https://www.youtube.com/embed/${data.trailer.key}`}
                     title="Trailer"
                     allowFullScreen
