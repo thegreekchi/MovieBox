20420812f8f8115585167ae7624fd0c4083b91fd
Godspower Amanze <59035803+leo9947@users.noreply.github.com>
2025-05-09 13:56:39 +0100

    modified the home page movies-card component

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,7 +7,7 @@ const Movies = ({ movie, type }) => {
   return (
     <div className="">
       <Link to={`/${type}/${movie.id}`}>
-        <div className="min-w-36 h-44 sm:h-52 sm:min-w-44  md:min-w-52 md:h-60 cursor-pointer relative shadow-lg shadow-black rounded-md">
+        <div className="min-w-36 h-48 sm:h-56 sm:min-w-44  md:min-w-48 md:h-64 cursor-pointer relative shadow-md shadow-black rounded-md">
           <div className="absolute flex justify-center items-center bottom-[5%] right-[5%] ">
             <ConicRow percentage={`${Math.round(movie.vote_average * 10)}`} />
           </div>
