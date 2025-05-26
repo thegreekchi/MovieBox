383aac0d46301a22a97306469463b03a32143f8d
Godspower Amanze <59035803+leo9947@users.noreply.github.com>
2025-05-26 17:15:09 +0100

    modified bookmark icon

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -6,12 +6,12 @@ import { FaBookmark } from "react-icons/fa";
 
 const Movies = ({ movie, type }) => {
   return (
-    <div className="">
+    <div className="relative">
+      <div className="absolute text-white flex justify-center items-center top-[0%] right-[0%] bg-gray-900 opacity-90 px-[2px] pt-5 pb-2 z-10 border-2 rounded-sm rounded-tr-md border-white cursor-pointer">
+        <FaBookmark className="" />
+      </div>
       <Link to={`/media/${type}/${movie.id}`}>
         <div className="min-w-36 h-48 sm:h-56 sm:min-w-44  md:min-w-48 md:h-64 cursor-pointer relative shadow-md shadow-black rounded-md overflow-hidden">
-          <div className="absolute text-white flex justify-center items-center top-[0%] right-[0%] bg-gray-900 opacity-90 px-[2px] pt-5 pb-2 z-10 border-2 rounded-sm rounded-tr-md border-white cursor-pointer">
-            <FaBookmark className="" />
-          </div>
           <div className="absolute flex justify-center items-center bottom-[5%] right-[5%] ">
             <ConicRow percentage={`${Math.round(movie.vote_average * 10)}`} />
           </div>
