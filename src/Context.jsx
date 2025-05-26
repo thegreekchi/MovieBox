0df72adc61eb64d509644cce3c28594426aef145
Godspower Amanze <59035803+leo9947@users.noreply.github.com>
2025-05-26 17:06:23 +0100

    added bookmark icon

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,6 +1,8 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebaseConfig";
+import { auth, db } from "./firebaseConfig";
 import { createContext, useEffect, useState } from "react";
+import { deleteDoc, doc, serverTimestamp, setDoc } from "firebase/firestore";
+import toast from "react-hot-toast";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const AuthContext = createContext();
@@ -14,8 +16,39 @@ const Context = ({ children }) => {
     return () => unsubscribe();
   }, []);
 
+  const addbookmark = async (userEmail, movie) => {
+    const ref = doc(db, "users", userEmail, "bookmarks", movie.id.toString());
+    const movieData = {
+      id: movie.id,
+      title: movie.title,
+      img: movie.poster_path || movie.backdrop_path,
+      timeStamp: serverTimestamp(),
+    };
+    try {
+      await setDoc(ref, movieData);
+      return toast.success("Movie added to watchlist");
+    } catch (error) {
+      console.log(error);
+      return toast.error("Bookmark failed");
+    }
+  };
+
+  const removeBookmark = async (userEmail, movie) => {
+    const ref = doc(db, "users", userEmail, "bookmarks", movie.id.toString());
+
+    try {
+      await deleteDoc(ref);
+      return toast.success("Movie deleted");
+    } catch (error) {
+      console.log(error);
+      return toast.error("Delete failed");
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ isAuth }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ isAuth, addbookmark, removeBookmark }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
